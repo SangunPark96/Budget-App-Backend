299eb924c1e2fc80fc8db33e5815773adb086b77
SangUn Park <Sangunpark1@gmail.com>
2023-04-17 11:11:20 -0400

    added nodemon script

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const cors = require("cors")
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const budgetController = require("./controllers/budgetController.js")
 
   
 
 app.get("/", (req, res) => {
     res.send("welcome to the server")
 })
+
+app.use("/transactions", budgetController);
+
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Page not found" })
+  });
 
 
 module.exports = app
